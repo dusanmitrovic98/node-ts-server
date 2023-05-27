3d71bd3290deb574466d289ae79e714d8103a265
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-27 15:38:31 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
 import express from 'express';
-import { Request, Response } from 'express';
-
-const app = express();
-const port = 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
