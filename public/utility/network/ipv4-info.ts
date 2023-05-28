95fed52fc84bd00cd87c23547a4ffdd932a3d7da
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-28 15:02:43 +0200

    Update ipv4-info.ts

--- public/utility/network/ipv4-info.ts
+++ public/utility/network/ipv4-info.ts
@@ -1,6 +1,11 @@
 import { networkInterfaces } from 'os';
 
 function getIPv4Address(): string | undefined {
+    const interfaces = networkInterfaces();
+
+    for (const interfaceName in Object.keys(interfaces)) {
+        
+    }
     
     return undefined;
 }
