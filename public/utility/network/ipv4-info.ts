69259ddf49eba1da8f2ccf8c17b151e58b903021
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-28 15:13:58 +0200

    Update index.ts

--- public/utility/network/ipv4-info.ts
+++ public/utility/network/ipv4-info.ts
@@ -1,24 +1,20 @@
 import { networkInterfaces } from 'os';
 
 function getIPv4Address(): string | undefined {
-    const interfaces = networkInterfaces();
+  const interfaces = networkInterfaces();
 
-    for (const interfaceName in Object.keys(interfaces)) {
-        const addresses = interfaces[interfaceName];
-
-        if (addresses)
-        {
-            for (const address of addresses)
-            {
-                if (address.family == "IPv4" && !address.internal)
-                {
-                    return address.address;
-                }
-            }
+  for (const interfaceName of Object.keys(interfaces)) {
+    const addresses = interfaces[interfaceName];
+    if (addresses) {
+      for (const address of addresses) {
+        if (address.family === 'IPv4' && !address.internal) {
+          return address.address;
         }
+      }
     }
-    
-    return undefined;
+  }
+
+  return undefined;
 }
 
 export const ipv4 = getIPv4Address();
