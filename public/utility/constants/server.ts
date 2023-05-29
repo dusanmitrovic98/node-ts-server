3ce949e1b3bb930c1a9c07916408d818c5082b6b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-29 12:24:59 +0200

    Updated server.ts

--- public/utility/constants/server.ts
+++ public/utility/constants/server.ts
@@ -6,8 +6,8 @@ export const PORT: number = 3000;
 
 // Directories
 export const DIRECTORY_NAME_PUBLIC: string = "public";
-export const RELATIVE_PATH_VIEW_INDEX: string = "./views/index.html";
+export const PATH_VIEW_INDEX: string = "./views/index.html";
 
 // SSl
-export const SSL_KEY: string = "./ssl/private.key";
-export const SSL_CERT: string = "./ssl/certificate.crt";
+export const PATH_SSL_KEY: string = "./ssl/private.key";
+export const PATH_SSL_CERT: string = "./ssl/certificate.crt";
