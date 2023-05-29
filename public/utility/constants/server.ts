63828e6f4d392ee1bfc8dd7a5ef4ef77e230583d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-29 12:17:11 +0200

    Added constant

---
+++ public/utility/constants/server.ts
@@ -0,0 +1,8 @@
+import { ipv4 } from "../network/ipv4-info";
+
+// Server
+export const IP_ADDRESS: string = ipv4;
+export const PORT: number = 3000;
+
+// Directories
+export const DIRECTORY_PUBLIC: string = "public";
--- public/utility/constants/server/server.ts
+++
@@ -1,5 +0,0 @@
-import { ipv4 } from "../../network/ipv4-info";
-
-// Server
-export const IP_ADDRESS: string = ipv4;
-export const PORT: number = 3000;
