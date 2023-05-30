c5da9bf26577765cc8a672f8e0a859f0a4ffcdaf
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-30 10:55:54 +0200

    Update

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import { PORT, IP_ADDRESS, DIRECTORY_PUBLIC, PATH_VIEW_INDEX, PATH_SSL_KEY, PATH_SSL_CERT } from "./public/utility/constants/server";
+import { IP_ADDRESS, DIRECTORY_SOURCE, PATH_VIEW_INDEX, PATH_SSL_KEY, PATH_SSL_CERT } from "./src/utility/constants/server";
 import express, { Application, Request, Response } from "express";
 import * as path from "path"
 import https from 'https';
+import cors from "cors";
 import fs from 'fs';
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, DIRECTORY_PUBLIC)));
+app.use(express.static(path.join(__dirname, DIRECTORY_SOURCE)));
 
 const options = {
     key: fs.readFileSync(PATH_SSL_KEY),
@@ -18,11 +20,13 @@ const options = {
 app.get(
     "/",
     async (req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, DIRECTORY_PUBLIC, PATH_VIEW_INDEX));
+        res.sendFile(path.join(__dirname, DIRECTORY_SOURCE, PATH_VIEW_INDEX));
     }
 );
 
 try {
+    let PORT: number = parseInt(process.env.PORT || '3000', 10);
+
     https.createServer(options, app).listen(PORT, IP_ADDRESS + "", (): void => {
         console.log(`Server url: https://${IP_ADDRESS}:${PORT}`);
     });
--- src/utility/constants/server.ts
+++ src/utility/constants/server.ts
@@ -2,10 +2,9 @@ import { ipv4 } from "../network/ipv4-info";
 
 // Server
 export const IP_ADDRESS: string = ipv4;
-export const PORT: number = 3000;
 
 // Directories
-export const DIRECTORY_PUBLIC: string = "public";
+export const DIRECTORY_SOURCE: string = "src";
 export const PATH_VIEW_INDEX: string = "./views/index.html";
 
 // SSl
