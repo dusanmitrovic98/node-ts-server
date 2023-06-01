ce38414309b278793cdce61fa22a9b027e96a0c1
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 11:35:00 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import * as path from "path";
 import dotenv from "dotenv";
 import https from "https";
