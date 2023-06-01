8fe918a99a91457ac9b77a7af25c9b96a31bbcf2
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 14:08:29 +0200

    Update

--- src/utility/constants/server-constants.ts
+++ src/utility/constants/server-constants.ts
@@ -1,9 +1,4 @@
 import { ipv4 } from "../network/ipv4-info";
 
-// Server
 export const DEFAULT_IP_ADDRESS: string = ipv4;
 export const DEFAULT_PORT: string = "3000";
-
-// SSl
-export const PATH_SSL_KEY: string = "./ssl/private.key";
-export const PATH_SSL_CERT: string = "./ssl/certificate.crt";
---
+++ src/utility/constants/ssl-constants.ts
@@ -0,0 +1,2 @@
+export const PATH_SSL_KEY: string = "./ssl/private.key";
+export const PATH_SSL_CERT: string = "./ssl/certificate.crt";
