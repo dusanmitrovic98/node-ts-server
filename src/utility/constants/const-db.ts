6dd16d8e5ec32ed28308c247555b0693c48c4345
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 18:02:47 +0200

    Update const-db.ts

--- src/utility/constants/const-db.ts
+++ src/utility/constants/const-db.ts
@@ -1,4 +1,4 @@
 import { ipv4 } from "../network/ipv4-info";
 
 export const DEFAULT_DB_HOST: string = ipv4;
-export const DEFAULT_DB_PORT: number = 5432;
+export const DEFAULT_DB_PORT: number = 27017;
