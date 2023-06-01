9c17b74903b625b9023e996095b46f54a165e6ec
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 12:09:29 +0200

    Update authentication.ts

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,1 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+
+export function authenticate(req: Request, res: Response, next: NextFunction) {
+  // Check if the request contains a valid authentication token/session
+  // If authenticated, call next() to proceed to the next middleware/route handler
+  // If not authenticated, send an error response or redirect to a login page
+}
