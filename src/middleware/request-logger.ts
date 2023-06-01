159e99b33c286a50df6c3a76ceb7eb8139e48dcb
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 12:17:30 +0200

    Update request-logger.ts

--- src/middleware/request-logger.ts
+++ src/middleware/request-logger.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 
-export function requestLogger(req: Request, res: Response, next: NextFunction) {
-  // Log request details, such as method, URL, request body, headers, etc.
-  // This can be logged to the console, a file, or a logging service
+const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const { method, url, params, query, body } = req;
+
+  // Log the request information
+  console.log("Request:");
+  console.log("Method:", method);
+  console.log("URL:", url);
+  console.log("Params:", params);
+  console.log("Query:", query);
+  console.log("Body:", body);
+
+  // Proceed to the next middleware or route handler
   next();
-}
+};
+
+export default requestLogger;
