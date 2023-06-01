706c57a1eced2477fd8b34726e54cbaf21b4cb36
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 18:12:15 +0200

    Update database.ts

--- database.ts
+++ database.ts
@@ -2,4 +2,6 @@ import { MongoClient, Db } from "mongodb";
 
 let db: Db;
 
-export async function connectDatabase(): Promise<void> {}
+export async function connectDatabase(): Promise<void> {
+  const url = "mongodb://localhost:27017";
+}
