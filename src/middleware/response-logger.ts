427ec3da330e1353960ecd97224ce3131eb2f739
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 13:38:07 +0200

    Update response-logger.ts

--- src/middleware/response-logger.ts
+++ src/middleware/response-logger.ts
@@ -1,22 +1,17 @@
 import { Request, Response, NextFunction, Send } from "express";
 
 const responseLogger = (req: Request, res: Response, next: NextFunction) => {
-  // Store the original res.send function
   const originalSend: Send = res.send;
 
-  // Override the res.send function
   res.send = function (body?: any): any {
-    // Log the response details
     console.log("Response:", {
       statusCode: res.statusCode,
       body,
     });
 
-    // Call the original res.send function
     return originalSend.apply(res, arguments as any);
   };
 
-  // Continue to the next middleware
   next();
 };
 
