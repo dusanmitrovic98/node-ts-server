a6c3171601269e125dda046c50272047a7082eff
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-01 12:10:31 +0200

    Update authorization.ts

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,1 +1,9 @@
 import { Request, Response, NextFunction } from "express";
+
+export function authorize(roles: string[]) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    // Check if the authenticated user has the required roles/permissions
+    // If authorized, call next() to proceed to the next middleware/route handler
+    // If not authorized, send a forbidden error response
+  };
+}
